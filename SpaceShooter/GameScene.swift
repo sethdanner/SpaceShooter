ba848ce09a3628f85b3f109520ca776dd3fe207d
Seth Danner <sethdanner@Seths-iMac.local>
2019-03-23 21:17:18 -0600

    Fixed GameScene.sks anchor point and added Score Label.

--- SpaceShooter/GameScene.swift
+++ SpaceShooter/GameScene.swift
@@ -9,19 +9,46 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var starfield: SKEmitterNode!
     var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
         
+        
+        // MARK: Starfield
         starfield = SKEmitterNode(fileNamed: "Starfield")
         starfield.position = CGPoint(x: 0, y: 1472)
         starfield.advanceSimulationTime(10)
         self.addChild(starfield)
-        
         starfield.zPosition = -1
+        
+        // MARK: Player
+        player = SKSpriteNode(imageNamed: "shuttle")
+//        player.position = CGPoint(x: self.frame.size.width / 2, y: player.size.height / 2 + 20)
+        player.position = CGPoint(x: self.frame.size.width / 2, y: player.size.height / 2 + 20)
+        self.addChild(player)
+        
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
+        
+        // MARK: Score Label
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: 100, y: self.frame.size.height - 60)
+        scoreLabel.fontName = "IowanOldStyle-Bold"
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = UIColor.white
+        score = 0
+        self.addChild(scoreLabel)
+        
+        
     }
     
     
